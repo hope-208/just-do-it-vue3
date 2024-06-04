4037f1978b333711dae4b7a0fab2a197ef397364
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-06-05 01:44:33 +0400

    Bugfix build for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/just-do-it-vue3/',
   plugins: [
     vue(),
     vueJsx(),
